a3b666b118b31779cd8bf1e73567695391f93f60
Prajkmr <prajwal.koretagere.vijay.kumar@tuhh.de>
2023-07-18 00:44:50 +0200

    commit

---
+++ stock_analyzer_app/untitled folder/stock_analysis_template.R
@@ -0,0 +1,93 @@
+# Business Analytics with Data Science and Machine Learning ----
+# Building Business Data Products ----
+# STOCK ANALYZER APP - DATA ANALYSIS -----
+
+# APPLICATION DESCRIPTION ----
+# - The user will select 1 stock from the SP 500 stock index
+# - The functionality is designed to pull the past 180 days of stock data by default
+# - We will implement 2 moving averages - short (fast) and long (slow)
+# - We will produce a timeseries visualization
+# - We will produce automated commentary based on the moving averages
+
+# LIBRARIES ----
+library(tidyverse)
+library(fs)
+library(glue)
+
+library(rvest)
+library(quantmod)
+
+library(plotly)
+
+# 1.0 GET STOCK LIST ----
+# 1.0 GET STOCK LIST ----
+get_stock_list <- function(stock_index = "DAX") {
+  
+  # Control for upper and lower case
+  index_lower <- str_to_lower(stock_index)
+  # Control if user input is valid
+  index_valid <- c("dax", "sp500", "dow", "nasdaq")
+  if (!index_lower %in% index_valid) {stop(paste0("x must be a character string in the form of a valid exchange.",
+                                                  " The following are valid options:\n",
+                                                  stringr::str_c(str_to_upper(index_valid), collapse = ", ")))
+  }
+  
+  # Control for different currencies and different column namings in wiki
+  vars <- switch(index_lower,
+                 dax    = list(wiki     = "DAX", 
+                               columns  = c("Ticker symbol", "Company")),
+                 sp500  = list(wiki     = "List_of_S%26P_500_companies", 
+                               columns  = c("Symbol", "Security")),
+                 dow    = list(wiki     = "Dow_Jones_Industrial_Average",
+                               columns  = c("Symbol", "Company")),
+                 nasdaq = list(wiki     = "NASDAQ-100",
+                               columns  = c("Ticker", "Company"))
+  )
+  
+  # Extract stock list depending on user input
+  read_html(glue("https://en.wikipedia.org/wiki/{vars$wiki}")) %>% 
+    
+    # Extract table from wiki
+    html_nodes(css = "#constituents") %>% 
+    html_table() %>% 
+    dplyr::first() %>% 
+    as_tibble(.name_repair = "minimal") %>% 
+    # Select desired columns (different for each article)
+    dplyr::select(vars$columns) %>% 
+    # Make naming identical
+    set_names(c("symbol", "company")) %>% 
+    
+    # Clean (just relevant for DOW)
+    mutate(symbol = str_remove(symbol, "NYSE\\:[[:space:]]")) %>% 
+    
+    # Sort
+    arrange(symbol) %>%
+    # Create the label for the dropdown list (Symbol + company name)
+    mutate(label = str_c(symbol, company, sep = ", ")) %>%
+    dplyr::select(label)
+  
+}
+
+
+# 2.0 EXTRACT SYMBOL BASED ON USER INPUT ----
+
+
+
+# 3.0 GET STOCK DATA ----
+
+
+
+# 4.0 PLOT STOCK DATA ----
+
+
+
+# 5.0 GENERATE COMMENTARY ----
+
+
+
+# 6.0 TEST WORKFLOW ----
+
+
+
+# 7.0 SAVE SCRIPTS ----
+
